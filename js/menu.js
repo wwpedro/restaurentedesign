2ecb568e521efaacee251251bec64e3a43213156
wwpedro <pedrinhosilva35997@gmail.com>
2023-01-11 15:50:13 -0300

    Feita a pagina sobre

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function click_menu(){
+
+    if(document.getElementById("itens_menu").style.display="block"){
+        document.getElementById("itens_menu").style.display = "none";
+    }else{
+        document.getElementById("itens_menu").style.display = "block";
+    }
+}
